30f91fdd686bc6094a5398b6c9ed1a2460e25078
Matteogatto94 <cosenza94@live.it>
2023-01-17 15:37:07 +0100

    create model, controller and seeder

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Technology;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $technologies = ['CSS', 'HTML', 'JS', 'PHP', 'LARAVEL', 'MYSQL'];
+
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($technology);
+            $newTechnology->save();
+        }
+    }
+}
